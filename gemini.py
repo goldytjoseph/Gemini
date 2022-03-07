29dc280d6e9d83bef0088f851bcf8852e58c9f17
Goldy T Joseph <35062235+goldytjoseph@users.noreply.github.com>
2022-03-07 08:24:05 +0300

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 # *** coding: utf-8 ***
-# *** Author: Goldy Thundiyil Joseph ***
-# *** Gemini is a program which generates thumbnail from image. ***
-
 
 from PIL import Image
 import os
